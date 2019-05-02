eab2a9d29d21568112ff2baf0a669bc8c8aa7feb
ahn <ajs0904@wezon.co.kr>
2019-05-02 18:31:18 +0900

    2019-05-02 18

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,54 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-          id: 1,
-          image: 'https://placeimg.com/64/64/1',
-          name: 'dreamer',
-          birthday: '860904',
-          gender: 'man',
-          job: 'dev'
-        },
-        {
-          id: 2,
-          image: 'https://placeimg.com/64/64/2',
-          name: 'apple',
-          birthday: '860904',
-          gender: 'man',
-          job: 'dev'
-        },
-        {
-          id: 3,
-          image: 'https://placeimg.com/64/64/3',
-          name: 'orange',
-          birthday: '860904',
-          gender: 'man',
-          job: 'dev'
+    connection.query(
+        "SELECT * FROM customer",
+        (err, rows, fields) => {
+            res.send(rows);
         }
-    ]);
+
+    )
+
+    // res.send([
+    //     {
+    //       id: 1,
+    //       image: 'https://placeimg.com/64/64/1',
+    //       name: 'dreamer',
+    //       birthday: '860904',
+    //       gender: 'man',
+    //       job: 'dev'
+    //     },
+    //     {
+    //       id: 2,
+    //       image: 'https://placeimg.com/64/64/2',
+    //       name: 'apple',
+    //       birthday: '860904',
+    //       gender: 'man',
+    //       job: 'dev'
+    //     },
+    //     {
+    //       id: 3,
+    //       image: 'https://placeimg.com/64/64/3',
+    //       name: 'orange',
+    //       birthday: '860904',
+    //       gender: 'man',
+    //       job: 'dev'
+    //     }
+    // ]);
 });
 app.get('/api/hello', (req, res) => {
     res.send({message: 'hello Express!'});
