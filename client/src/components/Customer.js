4a7903008c3f4f2807b8bf2f69560639af01d839
ahn <ajs0904@wezon.co.kr>
2019-05-02 14:38:11 +0900

    2019-05-02 14
    2019-05-02 14

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Customer extends Component {
   render() {
     return (
-        <div>
-            <CustomerProfile
-                id={this.props.id}
-                image={this.props.image}
-                name={this.props.name}
-            ></CustomerProfile>
-            <CustomerInfo
-                birthday={this.props.birthday}
-                gender={this.props.gender}
-                job={this.props.job}
-            ></CustomerInfo>
-        </div>
+        <TableRow>
+            <TableCell>{this.props.id}</TableCell>
+            <TableCell><img src={this.props.image} alt="profile"></img></TableCell>
+            <TableCell>{this.props.name}</TableCell>
+            <TableCell>{this.props.birthday}</TableCell>
+            <TableCell>{this.props.gender}</TableCell>
+            <TableCell>{this.props.job}</TableCell>
+        </TableRow>
     );
   }
 }
